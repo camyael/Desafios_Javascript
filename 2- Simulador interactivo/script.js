5021c505cdefbe601af45599399c12df3dcd962b
Camila Villaverde <camilavillaverde3@gmail.com>
2022-07-25 20:19:58 -0300

    nuevos cambios

--- 2- Simulador interactivo/script.js
+++ 2- Simulador interactivo/script.js
@@ -1,88 +1,84 @@
-/*let precio, opciones
-
-do {
-    precio = parseFloat(prompt("Ingrese el precio para saber si tiene descuento"))
-    console.log(`El precio del producto sin descuento es ${precio}`)
-
-    if ((precio > 0) && (precio <= 1000)) {
-        console.log(`Este producto tiene un 20% de descuento`)
-        opciones = parseInt(prompt(`Ingrese las siguientes opciones (solamente el numero):\n\n1) Si quiere saber el precio final del producto\n2) Si quiere salir`))
-        let descuento20 = precio - (precio * 20 / 100)
-
-        switch (opciones) {
-            case 1:
-                console.log(`¡Su producto con el 20% saldría ${descuento20}!`)
-                break;
-            case 2:
-                console.log(`Hasta pronto<3`)
-                break; 
-            default:
-                break;
-        }
-    } else if (precio > 1000){
-        console.log(`Este producto tiene un 30% de descuento`)
-        opciones = parseInt(prompt(`Ingrese las siguientes opciones (solamente el numero):\n\n1) Si quiere saber el precio final del producto\n2) Si quiere salir`))
-        let descuento30 = precio - (precio * 30 / 100)
-
-        switch (opciones) {
-            case 1:
-                console.log(`¡Su producto con el 30% saldría ${descuento30}!`)
-                break;
-            case 2:
-                console.log(`Hasta pronto<3`)
-                break; 
-            default:
-                break;
-        }
-    } else if (precio === 0) {
-        console.log(`El precio no es valido`)
-    } else {
-        alert("El producto no cuenta con ningun descuento, esperamos que vuelva pronto")
-    }
-} while (opciones != 2)
-*/
-
 alert("Usted está por entrar al simulador de Hoyts para comprar entradas")
 
-let simulador = true 
+//FUNCIONES
+function precioEntradas(cantidadEntrada, sala) {
+    return cantidadEntrada * sala
+}
 
-while(simulador){
+let simulador = true
+
+while(simulador) {
     let nombre = prompt("¿Cuál es su nombre? Por favor, escribir nombre completo")
     console.log(`¡Bienvenido/a ${nombre}!`)
 
     let cine
     do {
+        //eligiendo cine
         cine = prompt("Ingrese el Cine al que quiere ir: \n\nCinemark Avellaneda\nCinemark Palermo\nCinemark Puerto Madero\nHoyts Abasto").toLowerCase()
 
         if((cine == "cinemark avellaneda") || (cine == "cinemark palermo") || (cine == "cinemark puerto madero") || (cine == "hoyts abasto")){
             console.log(`${nombre} ha seleccionado ${cine}`)
+            //pelicula
+            let pelicula = prompt("¿Que pelicula desea ver? En cartelera esta: \n\nJurassic World Dominio\nMinions\nThor Amor y Trueno\nLightyear\nTop Gun Maverick").toLowerCase()
 
-            
+            if ((pelicula == "jurassic world dominio") ||(pelicula == "minions") || (pelicula == "thor amor y trueno") || (pelicula == "lightyear") || (pelicula == "top gun maverick")) {
+                console.log(`Usted está por comprar entradas para ver la pelicula ${pelicula} en ${cine}`)
 
+                //valor de entradas
+                let entrada2d = 940
+                console.log(`La entrada a las Salas 2D salen $${entrada2d}`)
+                let entrada3d = 1060
+                console.log(`La entrada a las Salas 3D salen $${entrada3d}`)
+
+                // cantidad de entradas
+                let entrada = parseInt(prompt("Ingrese la cantidad de entradas, puede comprar hasta 6 entradas en total"))
+                console.log(`${entrada} entrada/s`)
+                if (entrada <= 6) {
+                    //salas
+                    let salaCine = parseInt(prompt("¿Desea las Salas 2D o las Salas 3D? Escriba 1 o 2 segun corresponda"))
+                    if (salaCine = 1) {
+                        //SALA 2D
+                        let resultado = precioEntradas(entrada, entrada2d)
+                        console.log(`$${resultado}`)
+                        alert(`${nombre}, esta por comprar ${entrada} entrada/s en 2D (cada entrada sale $${entrada2d}) para ver ${pelicula} en ${cine} \n\nEl total seria $${resultado}`)
+                    } else if (salaCine = 2) {
+                        //SALA 3D 
+                        let resultado = precioEntradas(entrada, entrada3d)
+                        console.log(`$${resultado}`)
+                        alert(`${nombre}, esta por comprar ${entrada} entrada/s en 3D (cada entrada sale $${entrada3d}) para ver ${pelicula} en ${cine} \n\nEl total seria $${resultado}`)
+                    } else {
+                        alert("No contamos con esa sala")
+                    }
+
+                    let continuar = prompt("¿Desea continuar?")
+                    if(continuar == "no"){
+                        simulador = false
+                    }
+
+                } else {
+                    alert("Solo puede comprar hasta 6 entradas")
+                }
+
+            } else {
+                alert("Error, pelicula no existente")
+            }
 
         }else {
             console.log(`Cine no encontrado`)
-            alert("Por favor, escribir el cine como dice anteriormente")
+            alert("Por favor, escribir el nombre del cine como dice anteriormente")
         }
 
-       /* let continuar
-        do {
-            continuar = prompt("¿Desea continuar comprando más entradas?").toLowerCase()
-            switch (continuar) {
-                case "si":
-                    console.log("si")
-                    break;
-                
-                case no:
-                    simulador = false
-                    console.log("no")
-                    break;
-        
-                default:
-                    alert("Respuesta no valida")
-                    break;
+        let continuar = prompt("¿Desea continuar?")
+            if(continuar == "no"){
+                simulador = false
             }
-        } while ((continuar != "si") || (continuar != "no"));*/
-    
+
     } while ((cine != "cinemark avellaneda") || (cine != "cinemark palermo") || (cine != "cinemark puerto madero") || (cine != "hoyts abasto"));
+
+    let continuar = prompt("¿Desea continuar?").toLowerCase()
+        if(continuar == "no"){
+            simulador = false
+        }
 }
+
+
